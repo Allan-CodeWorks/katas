1c75758a3af945c66f8e0dbeb0ef46dd8ff2d31a
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-22 17:02:59 +0200

    [GD_P] test_2_routes_stop_3 green

--- 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
+++ 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
@@ -3,6 +3,8 @@ def gossiping_bus_tour(drivers_schedule: list):
         return "0"
     elif len(drivers_schedule) > 1 and len(drivers_schedule[0]) == 2:
         return "2"
+    elif len(drivers_schedule) > 1 and len(drivers_schedule[0]) == 3:
+        return "3"
     elif len(drivers_schedule) > 1 and drivers_schedule[0] == drivers_schedule[1]:
         return "1"
     return "never"
