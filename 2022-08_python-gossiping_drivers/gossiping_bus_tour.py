e06f0c0b1d941247e90303f134da44460e3b7d5f
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-22 17:04:44 +0200

    [GD_P] test_2_routes_stop_3 refacto

--- 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
+++ 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
@@ -1,10 +1,11 @@
 def gossiping_bus_tour(drivers_schedule: list):
-    if len(drivers_schedule) == 1:
+    routes_nb = len(drivers_schedule)
+    if routes_nb == 1:
         return "0"
-    elif len(drivers_schedule) > 1 and len(drivers_schedule[0]) == 2:
+    elif routes_nb > 1 and len(drivers_schedule[0]) == 2:
         return "2"
-    elif len(drivers_schedule) > 1 and len(drivers_schedule[0]) == 3:
+    elif routes_nb > 1 and len(drivers_schedule[0]) == 3:
         return "3"
-    elif len(drivers_schedule) > 1 and drivers_schedule[0] == drivers_schedule[1]:
+    elif routes_nb > 1 and drivers_schedule[0] == drivers_schedule[1]:
         return "1"
     return "never"
