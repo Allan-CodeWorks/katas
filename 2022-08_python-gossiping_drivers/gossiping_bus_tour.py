bd9ab9912d2e165cd8020ae0fbdadce0e93bac19
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-22 16:15:19 +0200

    [GD_P] test_2_routes_never green

--- 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
+++ 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
@@ -1,6 +1,6 @@
 def gossiping_bus_tour(drivers_schedule: list):
     if len(drivers_schedule) == 1:
         return "0"
-    elif len(drivers_schedule) >= 1:
+    elif len(drivers_schedule) > 1 and drivers_schedule[0] == drivers_schedule[1]:
         return "1"
     return "never"
