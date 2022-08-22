c635b53a14cdc0236e4cabeaddfb52518686cad6
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-22 16:04:44 +0200

    [GD_P] test 1 route refacto

--- 2022-08_python-gossiping_drivers/gossiping_bus_tour_test.py
+++ 2022-08_python-gossiping_drivers/gossiping_bus_tour_test.py
@@ -1,12 +1,9 @@
 from gossiping_bus_tour import gossiping_bus_tour
 
 
-def test_dummy():
-    assert(3 == 3)
-
-
 def test_no_bus_today():
-    gossiping_done = gossiping_bus_tour([])
+    routes = []
+    gossiping_done = gossiping_bus_tour(routes)
     assert(gossiping_done == "never")
 
 
