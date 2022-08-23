26cd65982b910fc3f7f7c01804ca4ace7eae54c5
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-23 09:27:17 +0200

    [GD_P] test_2_routes_stop_1_uneven green

--- 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
+++ 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
@@ -1,10 +1,18 @@
+
+
 def gossiping_bus_tour(drivers_schedule: list):
     routes_nb = len(drivers_schedule)
     drivers_schedule
+
     if routes_nb == 1:
         return "0"
     elif routes_nb > 1 and drivers_schedule[0] == drivers_schedule[1]:
         return "1"
-    elif routes_nb > 1 and len(drivers_schedule[0]) > 1:
-        return str(len(drivers_schedule[0]))
+
+    drivers_locations_list = list(zip(*drivers_schedule))
+
+    for i, locs in enumerate(drivers_locations_list):
+        if len(set(locs)) == 1:
+            return str(i + 1)
+
     return "never"
