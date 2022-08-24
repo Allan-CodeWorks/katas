8efe38bbb50ae0e3889b61e2b62a70f9ccf9248b
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-24 11:05:53 +0200

    [GD_P] test_3_routes_never refacto 4

--- 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
+++ 2022-08_python-gossiping_drivers/gossiping_bus_tour.py
@@ -1,3 +1,30 @@
+def gossiping_bus_tour(schedule: list):
+    drivers_nb = len(schedule)
+    gossips = [set([i]) for i in range(drivers_nb)]
+
+    if solved(drivers_nb, gossips):
+        return "0"
+
+    stops_at_minute = list(zip(*schedule))
+
+    for minute, stops in enumerate(stops_at_minute):
+
+        drivers_at_stop = get_drivers_at_stop(stops)
+
+        for drivers in drivers_at_stop.values():
+            if len(drivers) > 1:
+                for driver in drivers:
+                    gossips[driver] = gossips[driver].union(drivers)
+
+        if solved(drivers_nb, gossips):
+            return str(minute + 1)
+
+    if is_drivers_never_cross(stops_at_minute):
+        return "never"
+
+    return "never"
+
+
 def is_all_drivers_at_same_stop(locations: tuple):
     s = set(locations)
     return len(set(locations)) == 1
@@ -24,23 +51,9 @@ def solved(drivers_nb, gossips):
     return True
 
 
-def gossiping_bus_tour(schedule: list):
-    drivers_nb = len(schedule)
-    gossips = [set([i]) for i in range(drivers_nb)]
-
-    if solved(drivers_nb, gossips):
-        return "0"
-
-    stops_at_minute = list(zip(*schedule))
-
-    for minute, locs in enumerate(stops_at_minute):
-        if solved(drivers_nb, gossips):
-            return minute + 1
-        if is_all_drivers_at_same_stop(locs):
-            return str(minute + 1)
-    if is_drivers_never_cross(stops_at_minute):
-        return "never"
-    if drivers_nb == 3:
-        return "4"
-
-    return "never"
+def get_drivers_at_stop(stops: tuple):
+    drivers_at_stop = dict([(stop, set()) for stop in stops])
+    for driver in range(len(stops)):
+        stop = stops[driver]
+        drivers_at_stop[stop].add(driver)
+    return drivers_at_stop
