2f334c73aaa824f4fdd4f16f57e5f76c519b078c
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-29 14:47:55 +0200

    [RPN] test_simple_addition_2000 refacto

--- 2022_08_python-rpn-calculator/rpn_calculator.py
+++ 2022_08_python-rpn-calculator/rpn_calculator.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from cmath import nan
 from curses.ascii import isdigit
 from tokenize import Number
@@ -7,15 +8,17 @@ def rpn(expression: str):
     if expression.isdigit():
         return int(expression)
     elif isRpnSequence(expression):
-        if "+" in expression:
-            if "50 2000" in expression:
-                return 13450
-            elif "2000" in expression:
-                return 2000
-            return 20
         e1, e2, op = expression.split(" ")
-        return int(e1) / int(e2)
+        return calculator(int(e1), int(e2), op)
     raise("Invalid input")
+
+
+def calculator(e1: int, e2: int, op: str):
+    if op == "+":
+        return e1 + e2
+    if op == "/":
+        return e1 / e2
+    return 0
 
 
 def isRpnOperator(operator: str):
