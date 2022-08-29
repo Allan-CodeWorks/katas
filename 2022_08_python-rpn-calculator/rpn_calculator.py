1df1acfb687b454e65b41858d642597e464b4170
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-29 18:13:51 +0200

    [RPN] test_recursive_rpn_27 green

--- 2022_08_python-rpn-calculator/rpn_calculator.py
+++ 2022_08_python-rpn-calculator/rpn_calculator.py
@@ -6,19 +6,36 @@ from tokenize import Number
 
 
 def rpn(expression: str):
+    members = expression.split(" ")
 
     if expression.isdigit():
         return int(expression)
-    elif isRpnFullSequence(expression):
-        e1, e2, op = expression.split(" ")
+    while isRpnComposed(members):
+
+        current_expression_index = find_first_full_sequence(members)
+        current_result = rpn(
+            str.join(" ", members[current_expression_index: current_expression_index + 3]))
+        expression = f'{members[0:current_expression_index]} {current_result} {expression[:current_expression_index + 3]}'
+        members = [member for i, member in enumerate(
+            members) if i != current_expression_index and i != current_expression_index + 1 and i != current_expression_index + 2]
+        members.insert(current_expression_index, str(current_result))
+
+    if isRpnFullSequence(members):
+        e1, e2, op = members
         return calculator(int(e1), int(e2), op)
-    if "-" in expression and "*" in expression:
-        return 27
-    if "+" in expression and "-" in expression:
-        return 3
-    if "*" in expression:
-        return 141
-    raise ValueError(expression)
+    else:
+        raise ValueError(expression)
+
+
+def find_first_full_sequence(members):
+    for i in range(len(members)):
+        if isRpnFullSequence(members[i:i + 3]):
+            return i
+    raise ValueError(f'Invalid sequence: no RPN expression found')
+
+
+def isRpnComposed(members: list):
+    return len(members) > 3
 
 
 def calculator(e1: int, e2: int, op: str):
@@ -39,8 +56,7 @@ def isRpnOperator(operator: str):
     return len(containsRpnOperator) > 0
 
 
-def isRpnFullSequence(expression: str):
-    members = expression.split(" ")
+def isRpnFullSequence(members: list):
     if len(members) > 3:
         return False
     e1, e2, op = members
