60250506864aa1b0fa3da4edf05666edc319a6ce
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-30 14:54:25 +0200

    [RPN] test_multiplication green

--- 2022-08_2-python-rpn-calculator/rpn.py
+++ 2022-08_2-python-rpn-calculator/rpn.py
@@ -9,6 +9,8 @@ def rpn(expression: str):
             stack.append(int(token))
         elif token == "+":
             stack.append(stack.pop() + stack.pop())
+        elif token == "*":
+            stack.append(stack.pop() * stack.pop())
         else:
             raise(ValueError)
     return stack.pop()
