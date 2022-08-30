d1378c9e3ab705b2693a42a9b8fa084555a0167a
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-08-30 15:07:35 +0200

    [RPN] test_division refacto1

--- 2022-08_2-python-rpn-calculator/rpn.py
+++ 2022-08_2-python-rpn-calculator/rpn.py
@@ -7,14 +7,20 @@ def rpn(expression: str):
     for token in tokens:
         if token.lstrip('-').isdigit():
             stack.append(int(token))
-        elif token == "+":
-            stack.append(stack.pop() + stack.pop())
-        elif token == "*":
-            stack.append(stack.pop() * stack.pop())
-        elif token == "/":
-            term1 = stack.pop()
-            term2 = stack.pop()
-            stack.append(term2 / term1)
+        elif isValidOperator(token):
+            if token == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif token == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif token == "/":
+                term1 = stack.pop()
+                term2 = stack.pop()
+                stack.append(term2 / term1)
         else:
             raise(ValueError)
     return stack.pop()
+
+
+def isValidOperator(operator: str):
+    validOperators = ['+', '*', '/']
+    return operator in validOperators
