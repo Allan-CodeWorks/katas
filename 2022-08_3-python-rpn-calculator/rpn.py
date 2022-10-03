9afdaf78222ab416b59ed6ab1d079111b0d45169
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-10-03 10:44:09 +0200

    IDK

--- 2022-08_3-python-rpn-calculator/rpn.py
+++ 2022-08_3-python-rpn-calculator/rpn.py
@@ -1,4 +1,11 @@
+class RpnNode:
+    def __init__(self) -> None:
+        pass
+
+
 def rpn(expression: str):
+    tokens = expression.split(' ')
+    tokens.reverse()
     if '+' in expression:
         return 5
     return int(expression)
