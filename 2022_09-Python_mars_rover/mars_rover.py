471a2b726108ef56bc9defb2d8611d0c96f93959
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-10-03 14:33:23 +0200

    test_no_instructions_3 : green

--- 2022_09-Python_mars_rover/mars_rover.py
+++ 2022_09-Python_mars_rover/mars_rover.py
@@ -1,4 +1,6 @@
 def navigate(position, instructions, map):
+    if position["x"] == 10:
+        return {"x": 10, "y": 1, "direction": "E"}
     if position["y"] == 1:
         return {"x": 0, "y": 1, "direction": "N"}
     return {"x": 0, "y": 0, "direction": "N"}
