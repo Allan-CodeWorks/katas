580959cdb56df0b8434882438357d27e7b866a99
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-10-03 16:37:18 +0200

    test_simple_forward_south_1 : green

--- 2022_09-Python_mars_rover/mars_rover.py
+++ 2022_09-Python_mars_rover/mars_rover.py
@@ -2,6 +2,10 @@ def navigate(position, instructions, map):
     if instructions:
         if position["direction"] == "N":
             position["y"] -= 1
+        elif position["direction"] == "S":
+            position["y"] += 1
+        elif position["direction"] == "W":
+            position["x"] -= 1
         else:
             position["x"] += 1
     return position
--- 2022_09-Python_mars_rover/mars_rover_test.py
+++ 2022_09-Python_mars_rover/mars_rover_test.py
@@ -54,18 +54,40 @@ def test_simple_forward_north(navigate_input_position, expected):
 
 
 positions_test_simple_forward_south = [
+    {"x": 5, "y": 0, "direction": "S"},
     {"x": 5, "y": 1, "direction": "S"},
-
+    {"x": 5, "y": 2, "direction": "S"}
 ]
 
 
 positions_test_simple_forward_south_expected = [
-    {"x": 4, "y": 0, "direction": "S"},
-
+    {"x": 5, "y": 1, "direction": "S"},
+    {"x": 5, "y": 2, "direction": "S"},
+    {"x": 5, "y": 3, "direction": "S"}
 ]
 
 
 @pytest.mark.parametrize("navigate_input_position, expected",
                          zip(positions_test_simple_forward_south, positions_test_simple_forward_south_expected))
 def test_simple_forward_south(navigate_input_position, expected):
+    assert(navigate(navigate_input_position, ["Forward"], None) == expected)
+
+
+positions_test_simple_forward_west = [
+    {"x": 5, "y": 1, "direction": "W"},
+    {"x": 6, "y": 1, "direction": "W"},
+    {"x": 7, "y": 1, "direction": "W"}
+]
+
+
+positions_test_simple_forward_west_expected = [
+    {"x": 4, "y": 1, "direction": "W"},
+    {"x": 5, "y": 1, "direction": "W"},
+    {"x": 6, "y": 1, "direction": "W"}
+]
+
+
+@pytest.mark.parametrize("navigate_input_position, expected",
+                         zip(positions_test_simple_forward_west, positions_test_simple_forward_west_expected))
+def test_simple_forward_west(navigate_input_position, expected):
     assert(navigate(navigate_input_position, ["Forward"], None) == expected)
