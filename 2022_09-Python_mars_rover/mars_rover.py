a2e016847b73b0636a92ea8157cb4409a4e18513
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-10-03 12:21:58 +0200

    test_no_instructions : Green

---
+++ 2022_09-Python_mars_rover/mars_rover.py
@@ -0,0 +1,2 @@
+def navigate(rover):
+    return {"x": 0, "y": 0, "direction": "N"}
---
+++ 2022_09-Python_mars_rover/mars_rover_test.py
@@ -0,0 +1,7 @@
+from mars_rover import navigate
+
+
+def test_no_instructions():
+    instructions = {"x": 0, "y": 0, "direction": "N"}
+    assert(navigate(instructions) == {
+        "x": 0, "y": 0, "direction": "N"})
