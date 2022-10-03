5c14ae3e1a7b022fe35d20863932b8c19a7f799e
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-10-03 15:12:07 +0200

    test_simple_forward_1 : red

--- 2022_09-Python_mars_rover/mars_rover_test.py
+++ 2022_09-Python_mars_rover/mars_rover_test.py
@@ -4,7 +4,7 @@ import pytest
 positions = [
     {"x": 0, "y": 0, "direction": "N"},
     {"x": 0, "y": 1, "direction": "N"},
-    {"x": 10, "y": 1, "direction": "E"}
+    {"x": 10, "y": 1, "direction": "E"},
 ]
 
 test_parameter_no_instructions = [
@@ -15,5 +15,10 @@ test_parameter_no_instructions = [
 
 
 @pytest.mark.parametrize("navigate_input_position, expected", test_parameter_no_instructions)
-def test_no_instructions(navigate_input_position, expected):
+def test_navigate_no_instructions(navigate_input_position, expected):
     assert(navigate(navigate_input_position, None, None) == expected)
+
+
+def test_simple_forward():
+    assert(navigate({"x": 0, "y": 0, "direction": "E"}, [
+           "Forward"], None) == {"x": 1, "y": 0, "direction": "E"})
