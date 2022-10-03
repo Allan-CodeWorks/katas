89c6184a39af7aa6db02072b3d36da35838414a1
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-10-03 17:21:44 +0200

    test_simple_forward_south_1 : refactroopeu

--- 2022_09-Python_mars_rover/mars_rover.py
+++ 2022_09-Python_mars_rover/mars_rover.py
@@ -1,12 +1,12 @@
 moves = {
-    "N": lambda position: {**position, "y": position["y"]-1},
-    "S": lambda position: {**position, "y": position["y"]+1},
-    "W": lambda position: {**position, "x": position["x"]-1},
-    "E": lambda position: {**position, "x": position["x"]+1},
+    'N': lambda position: {**position, 'y': position['y']-1},
+    'S': lambda position: {**position, 'y': position['y']+1},
+    'W': lambda position: {**position, 'x': position['x']-1},
+    'E': lambda position: {**position, 'x': position['x']+1},
 }
 
 
 def navigate(position, instructions, map):
     if instructions:
-        position = moves[position["direction"]](position)
+        position = moves[position['direction']](position)
     return position
