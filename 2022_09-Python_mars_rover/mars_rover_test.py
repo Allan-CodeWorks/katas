3622fc31eab169c6cc7d804819d984033c75d6bb
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-10-03 14:15:49 +0200

    test_simple_forward : red

--- 2022_09-Python_mars_rover/mars_rover_test.py
+++ 2022_09-Python_mars_rover/mars_rover_test.py
@@ -2,6 +2,13 @@ from mars_rover import navigate
 
 
 def test_no_instructions():
-    instructions = {"x": 0, "y": 0, "direction": "N"}
-    assert(navigate(instructions, None, None) == {
+    position = {"x": 0, "y": 0, "direction": "N"}
+    assert(navigate(position, None, None) == {
         "x": 0, "y": 0, "direction": "N"})
+
+
+def test_simple_forward():
+    position = {"x": 0, "y": 0, "direction": "E"}
+    instructions = ["Forward"]
+    assert(navigate(position, instructions, None)
+           == {"x": 0, "y": 1, "direction": "E"})
