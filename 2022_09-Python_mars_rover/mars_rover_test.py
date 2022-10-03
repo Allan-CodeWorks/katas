2f30b126c483228ed1ce41e2d6a47e67f6e02290
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-10-03 14:30:11 +0200

    test_no_instructions_2 : red

--- 2022_09-Python_mars_rover/mars_rover_test.py
+++ 2022_09-Python_mars_rover/mars_rover_test.py
@@ -11,3 +11,9 @@ def test_no_instructions_1():
     position = {"x": 0, "y": 1, "direction": "N"}
     assert(navigate(position, None, None) == {
         "x": 0, "y": 1, "direction": "N"})
+
+
+def test_no_instructions_2():
+    position = {"x": 10, "y": 1, "direction": "E"}
+    assert(navigate(position, None, None) == {
+        "x": 10, "y": 1, "direction": "E"})
