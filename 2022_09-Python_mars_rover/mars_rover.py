5031349a934ab098773655b5abb600368ffb6376
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-10-03 15:54:20 +0200

    test_simple_forward_3 : refacto_1

--- 2022_09-Python_mars_rover/mars_rover.py
+++ 2022_09-Python_mars_rover/mars_rover.py
@@ -1,6 +1,4 @@
 def navigate(position, instructions, map):
     if instructions:
-        if position["x"] == 3:
-            return {"x": 3, "y": 0, "direction": "N"}
-        return {"x": 1, "y": 0, "direction": "E"}
+        position["x"] += 1
     return position
--- 2022_09-Python_mars_rover/mars_rover_test.py
+++ 2022_09-Python_mars_rover/mars_rover_test.py
@@ -7,28 +7,34 @@ positions = [
     {"x": 10, "y": 1, "direction": "E"},
 ]
 
-test_parameter_no_instructions = [
-    (positions[0], positions[0]),
-    (positions[1], positions[1]),
-    (positions[2], positions[2])
-]
+
+def tables_to_tuples(table1, table2):
+    tuple = []
+    for i in range(len(table1)):
+        tuple.append((table1[i], table2[i]))
+    return tuple
 
 
-@pytest.mark.parametrize("navigate_input_position, expected", test_parameter_no_instructions)
+@pytest.mark.parametrize("navigate_input_position, expected", tables_to_tuples(positions, positions))
 def test_navigate_no_instructions(navigate_input_position, expected):
     assert(navigate(navigate_input_position, None, None) == expected)
 
 
-def test_simple_forward():
-    assert(navigate({"x": 0, "y": 0, "direction": "E"}, [
-           "Forward"], None) == {"x": 1, "y": 0, "direction": "E"})
+positions_test_simple_forward_east = [
+    {"x": 0, "y": 0, "direction": "E"},
+    {"x": 1, "y": 1, "direction": "E"},
+    {"x": 2, "y": 1, "direction": "E"}
+]
 
 
-def test_simple_forward_2():
-    assert(navigate({"x": 3, "y": 1, "direction": "N"}, [
-           "Forward"], None) == {"x": 3, "y": 0, "direction": "N"})
+positions_test_simple_forward_east_expected = [
+    {"x": 1, "y": 0, "direction": "E"},
+    {"x": 2, "y": 1, "direction": "E"},
+    {"x": 3, "y": 1, "direction": "E"}
+]
 
 
-def test_simple_forward_3():
-    assert(navigate({"x": 3, "y": 1, "direction": "S"}, [
-           "Forward"], None) == {"x": 3, "y": 2, "direction": "S"})
+@pytest.mark.parametrize("navigate_input_position, expected",
+                         tables_to_tuples(positions_test_simple_forward_east, positions_test_simple_forward_east_expected))
+def test_simple_forward_east(navigate_input_position, expected):
+    assert(navigate(navigate_input_position, ["Forward"], None) == expected)
